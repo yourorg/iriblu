9a2c40bc268a1a49dca36fefc59cbe061c7100f5
Martin Bramwell <martinhbramwell@gmail.com>
2017-09-23 11:42:38 -0400

    Enhance (api): MariaDb server prep scripts work on target or dev machine
    Signed-off-by: Martin Bramwell <martinhbramwell@gmail.com>

--- utils/target/mysql/LoadBackup.sh
+++ utils/target/mysql/LoadBackup.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-declare PRTY="\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nMariaDB -- Seeding :: ";
+declare PRTY="\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nMariaDB -- Seeding :: ";
 
-source ./secrets/secrets.sh;
+declare SECRETS=${1:-"./secrets/secrets.sh"};
+echo -e "${PRTY} ${SECRETS}";
+
+source ${SECRETS};
 
 if [[ -z ${RDBMS_BKP} ]]; then
   echo -e "${PRTY} No MariaDB seed URL was specified. Skipping ..."  | tee -a ${LOG};
 else
 
-  declare SEED_FILE="pgres_seed.sql";
+  declare SEED_FILE="database_seed.sql";
   echo -e "${PRTY} Downloading and restoring MariaDB seed from URL ...
                      ${RDBMS_BKP}
        ... to have internal standard name '${SEED_FILE}'"  | tee -a ${LOG};
@@ -19,7 +22,7 @@ else
 
   echo -e "${PRTY} Preparing database '${RDBMS_DB}' for user '${RDBMS_UID}' ..............";
 
-  declare CALL_MYSQL="mysql -u root -p${RDBMS_ADMIN_PWD} -e";
+  declare CALL_MYSQL="mysql -h ${RDBMS_HST} -u root -p${RDBMS_ADMIN_PWD} -e";
   # ${CALL_MYSQL} "SELECT User, Host from user;" mysql;
   ${CALL_MYSQL} "CREATE DATABASE IF NOT EXISTS ${RDBMS_DB};" mysql;
   ${CALL_MYSQL} "GRANT ALL PRIVILEGES ON ${RDBMS_DB}.* TO '${RDBMS_UID}'@'localhost' IDENTIFIED BY '${RDBMS_PWD}';" mysql;
@@ -35,7 +38,7 @@ else
     ";
   fi;
 
-  mysql -u ${RDBMS_UID} -p${RDBMS_PWD} -D ${RDBMS_DB} < ${SEED_FILE};
+  mysql -h ${RDBMS_HST} -u ${RDBMS_UID} -p${RDBMS_PWD} -D ${RDBMS_DB} < ${SEED_FILE};
   echo -e "${PRTY}  .............. imported seeding script.";
 
   echo -e "${PRTY} Done reseeding ...............";
