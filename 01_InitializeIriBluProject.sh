00adb1fb6657335b1e140f2304f1dec922cfe07e
Yourself YourOrg <yourself.yourorg@gmail.com>
2017-10-03 14:58:38 -0400

    Enhance (nvm):  Nvm installed cleanly
    Signed-off-by: Yourself YourOrg <yourself.yourorg@gmail.com>

--- 01_InitializeIriBluProject.sh
+++ 01_InitializeIriBluProject.sh
@@ -4,10 +4,6 @@ declare PRETTY="~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nIRIBLU Initialize :: ";
 
 sudo ls >/dev/null;
 
-echo -e "${PRETTY} prepare NodeJS versions ...";
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 export INSTALL="curl";
 dpkg -s ${INSTALL} >/dev/null && echo " - ${INSTALL} is installed" || sudo apt-get install -y ${INSTALL};
 
@@ -17,18 +13,27 @@ dpkg -s ${INSTALL} >/dev/null && echo " - ${INSTALL} is installed" || sudo apt-g
 export PURGE="nodejs";
 dpkg -s ${PURGE} &>/dev/null && sudo apt-get purge -y ${PURGE} || echo " - ${PURGE} has been purged";
 
+
+export NVM_DIR="$HOME/.nvm";
+echo -e "${PRETTY} prepare NodeJS versions ...";
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 export NVM_VERSION=$(curl -s https://api.github.com/repos/creationix/nvm/releases/latest | jq -r ".name");
-export NVM_INSTALLED=$(nvm --version);
+export NVM_INSTALLED=$(nvm --version 2>/dev/null);
 
 if [[  "${NVM_VERSION}" = "v${NVM_INSTALLED}"  ]]; then
   echo -e " - nvm '${NVM_VERSION}' is installed";
 else
   echo -e " - freshen nvm '${NVM_VERSION}'";
   wget -qO- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash;
-  export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi;
+export NVM_INSTALLED=$(nvm --version 2>/dev/null);
+echo -e " nvm installed version is ${NVM_INSTALLED}";
+echo -e "|||||||||||||||||||  curtailed  ||||||||||||||||||||||";
+exit;
 
 export NODE_VERSION=8;
 nvm ls ${NODE_VERSION} >/dev/null \
